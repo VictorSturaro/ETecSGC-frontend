ea97edddd0489f40b5db9192c2bc43d032631aa6
VictorSturaro <victor.quirino@etec.sp.gov.br>
2019-10-23 02:51:05 -0300

    finished commit

--- src/pages/Contagem/index.js
+++ src/pages/Contagem/index.js
@@ -1,13 +1,119 @@
-import React, { Style } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
+
+import "./styles.css"
 
 import '../../../src/App.css';
 
 export default function Contagem() {
+    const [contagem1, setContagem1] = useState([]);
+    const [contagem2, setContagem2] = useState([]);
+    const [contagem3, setContagem3] = useState([]);
+    const [contagem4, setContagem4] = useState([]);
+    const [contagem5, setContagem5] = useState([]);
+
+  useEffect(() => {
+    async function Contagem1() {
+      const response = await api.get('/contagem', {
+        headers : { dia: "Segunda-Feira" }
+      });
+
+      setContagem1(response.data);
+    }
+
+    Contagem1();
+  }, []);
+
+  useEffect(() => {
+    async function Contagem2() {
+      const response = await api.get('/contagem', {
+        headers : { dia: "Terça-Feira" }
+      });
+
+      setContagem2(response.data);
+    }
+
+    Contagem2();
+  }, []);
+
+  useEffect(() => {
+    async function Contagem3() {
+      const response = await api.get('/contagem', {
+        headers : { dia: "Quarta-Feira" }
+      });
+
+      setContagem3(response.data);
+    }
+
+    Contagem3();
+  }, []);
+
+  useEffect(() => {
+    async function Contagem4() {
+      const response = await api.get('/contagem', {
+        headers : { dia: "Quinta-Feira" }
+      });
+
+      setContagem4(response.data);
+    }
+
+    Contagem4();
+  }, []);
+
+  useEffect(() => {
+    async function Contagem5() {
+      const response = await api.get('/contagem', {
+        headers : { dia: "Sexta-Feira" }
+      });
+
+      setContagem5(response.data);
+    }
+
+    Contagem5();
+  }, []);
+
   return (
-    <div className="containerContagem">
-      <div className="contentContagem">
-      </div>
-    </div>
+    <>
+        <div className="containerContagem"> 
+            {contagem1.map(count => (
+                <div className="contentContagem" key={count._id}>
+                    <h1>{count._id}</h1><br />
+                    <h2>{count.total}</h2>
+                </div>
+            ))}
+
+            {contagem2.map(count => (
+                <div className="contentContagem" key={count._id}>
+                    <h1>{count._id}</h1><br />
+                    <h2>{count.total}</h2>
+                </div>
+            ))}
+
+            {contagem3.map(count => (
+                <div className="contentContagem" key={count._id}>
+                    <h1>{count._id}</h1><br />
+                    <h2>{count.total}</h2>
+                </div>
+            ))}
+
+            {contagem4.map(count => (
+                <div className="contentContagem" key={count._id}>
+                    <h1>{count._id}</h1><br />
+                    <h2>{count.total}</h2>
+                </div>
+            ))}
+
+            {contagem5.map(count => (
+                <div className="contentContagem" key={count._id}>
+                    <h1>{count._id}</h1><br />
+                    <h2>{count.total}</h2>
+                </div>
+            ))}
+        </div>
+        <Link to="/cardapios">
+             <button className='btnVoltar'>Voltar</button>
+        </Link>
+    </>
   )
 }
